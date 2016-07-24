717abc488330bb30e3b66544ed71f3d376cf3405
Greg Greenfield <g.r.greenfield@gmail.com>
2016-07-24 14:09:09 +1200

    Adding a bunch of classes to start building a calculator

--- src/calc/Mainclass.java
+++ src/calc/Mainclass.java
@@ -1,10 +1,11 @@
 package calc;
 
-public class main {
+public class Mainclass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Hello");
 
+		
 	}
 
 }
